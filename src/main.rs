953dedbb05f7e3857efa60ca73f438b632e2f782
williamtuominiemi <william.m.tuominiemi@gmail.com>
2025-09-27 00:56:19 +0300

    check for duplicate rules

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,23 @@ fn main() -> std::io::Result<()> {
     let lines: Vec<&str> = contents.split("\n").collect();
     println!("{:?}", lines);
     
+    let mut unique_lines = Vec::new();
+
     let mut row = 0;
     while row < lines.len() {
         let line = lines[row];
         if line != line.trim() {
             println!("Trailing whitespace on row {}", row);
-            println!("--> {}", line);
+            println!("--> |{}|", line);
         }
+
+        if unique_lines.contains(&line) {
+            println!("Duplicate rule on row {}", row);
+            println!("--> |{}|", line);
+        } else {
+            unique_lines.push(line);
+        }
+
         row += 1;
     }
 
