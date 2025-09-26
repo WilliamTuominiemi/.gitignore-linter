713937fecaa7ff4a56537b726fedadac4120efab
williamtuominiemi <william.m.tuominiemi@gmail.com>
2025-09-27 00:43:50 +0300

    find trailing whitespace

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,17 @@ fn main() -> std::io::Result<()> {
     file.read_to_string(&mut contents)?;
     let lines: Vec<&str> = contents.split("\n").collect();
     println!("{:?}", lines);
+    
+    let mut row = 0;
+    while row < lines.len() {
+        let line = lines[row];
+        if line != line.trim() {
+            println!("Trailing whitespace on row {}", row);
+            println!("--> {}", line);
+        }
+        row += 1;
+    }
+
     Ok(())
 }
+
